984faab636584c4eee7247b285f93d30369acc9c
Mostafa Tehrani <mostafa.tehrani.dev@gmail.com>
2024-03-30 22:49:26 +0200

    update query config file

--- utils/query_config.py
+++ utils/query_config.py
@@ -1,2 +1,11 @@
-can_write = False
-# can_write = True
+# from query_list import queries
+from query_list_test import queries
+
+# from query_list import queries
+from btn import write_header
+
+filepath = "../Law Clients.xlsm"
+
+# can_write = False
+log = True
+can_write = True
